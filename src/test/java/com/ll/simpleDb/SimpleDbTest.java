2e5146d54b96a4abcf35b0fdd5fabdd92e6bd795
seng-han <kumjjick@icloud.com>
2024-12-03 17:50:22 +0900

    db connection

--- src/test/java/com/ll/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -1,8 +1,6 @@
 package com.ll.simpleDb;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class SimpleDbTest {
@@ -11,6 +9,29 @@ public class SimpleDbTest {
     @BeforeAll
     public static void beforeAll() {
         simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
+
+        createArticleTable();
+    }
+
+    private static void createArticleTable() {
+        simpleDb.run("DROP TABLE IF EXISTS article");
+
+        simpleDb.run("""
+                CREATE TABLE article (
+                    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    PRIMARY KEY(id),
+                    createdDate DATETIME NOT NULL,
+                    modifiedDate DATETIME NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    `body` TEXT NOT NULL,
+                    isBlind BIT(1) NOT NULL DEFAULT 0
+                )
+                """);
+    }
+
+    @Test
+    @DisplayName("데이터베이스 연결 테스트")
+    public void t000() {
 
     }
 }
