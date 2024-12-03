14d705781ceded74a396ff3a957f10d61fd33b47
seng-han <kumjjick@icloud.com>
2024-12-03 17:35:17 +0900

    create SimpleDb class , b

--- src/main/java/com/ll/simpleDb/SimpleDb.java
+++ src/main/java/com/ll/simpleDb/SimpleDb.java
@@ -1,19 +1,11 @@
 package com.ll.simpleDb;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor //final붙은것들 자동으로 생성자 만들어짐
 public class SimpleDb {
     private final String host;
     private final String username;
     private final String password;
     private final String dbName;
-
-
-    public SimpleDb(String host, String username, String password, String dbName) {
-        this.host = host;
-        this.username = username;
-        this.password = password;
-        this.dbName = dbName;
-    }
-
-
 
 }
